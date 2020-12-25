3d0ac245d8016f42794aeaa3891744623a594457
Zain Shaikh <zainushaikh60@gmail.com>
2020-12-25 23:02:32 +0500

    'complete'

--- server.js
+++ server.js
@@ -17,6 +17,13 @@ app.use(express.urlencoded({ extended: true }));
 app.use('/todos', todosRouter);
 app.use('/todos-list', todosListRouter);
 
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  );
+}
+
 const port = config.get('PORT') || 4000;
 
 app.listen(port, () => {
