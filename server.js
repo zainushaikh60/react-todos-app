af5f27c67123ccc989976bb1d6c6e745bafa8e77
Zain Shaikh <zainushaikh60@gmail.com>
2020-12-25 22:47:54 +0500

    'complete'

--- server.js
+++ server.js
@@ -6,24 +6,18 @@ const todosRouter = require('./routes/todosRoutes');
 const todosListRouter = require('./routes/todosListRoutes');
 const path = require('path');
 
-const port = config.get('PORT') || 4000;
-
 // Connect DB
 connectDB();
 
-app.use(express.json({ extended: false }));
+app.use(express.json());
+
+app.use(express.urlencoded({ extended: true }));
 
 // routes
 app.use('/todos', todosRouter);
 app.use('/todos-list', todosListRouter);
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/build'));
-
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  });
-}
+const port = config.get('PORT') || 4000;
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
